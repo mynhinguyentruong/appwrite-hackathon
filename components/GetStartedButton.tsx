b450699c9e7a09dc7c91a58578f791e5dcee2206
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-05-30 12:35:19 -0400

    minor format edit

--- components/GetStartedButton.tsx
+++ components/GetStartedButton.tsx
@@ -7,14 +7,15 @@ export default function GetStartedButton() {
     const router = useRouter();
 
     async function getCredential() {
-        // fetch '/api/login'
-        // await router.push('/dashboard')
         const response = await fetch('/api')
         const client = await response.json()
         console.log({client})
         const account = new Account(client);
 
-        account.get().then(response => console.log({response})).catch(error => {
+        account
+            .get()
+            .then(response => console.log({response}))
+            .catch(error => {
             console.log({error})
             router.push('/dashboard')
         })
